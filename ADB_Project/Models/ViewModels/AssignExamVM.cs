9dc117a877a2c94995bc0d16a85d5e38cfb9715c
shahdm7md <shahd.m7mdd11@gmail.com>
2026-01-03 16:17:06 +0200

    assignExam added

--- ADB_Project/Models/ViewModels/AssignExamVM.cs
+++ ADB_Project/Models/ViewModels/AssignExamVM.cs
@@ -7,9 +7,9 @@ namespace ADB_Project.Models.ViewModels
         public int ExamId { get; set; }
         public bool AssignByDepartment { get; set; }
         public int? DepartmentId { get; set; }
-        public int[] SelectedStudentIds { get; set; }
+        public int[] SelectedStudentIds { get; set; } = new int[0];  // ✅ int[] not List<int>
         public DateTime? DueDate { get; set; }
-        public MultiSelectList Students { get; set; } // For view
-        public SelectList Departments { get; set; } // For view
+        public MultiSelectList? Students { get; set; }
+        public SelectList? Departments { get; set; }
     }
 }
